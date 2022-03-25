d326d4172eecdb593e8170569dee54149cc60dc1
Evgeniu <traingloo24@gmail.com>
2022-03-25 23:22:27 +0300

    Часть первого задания осталась валюта и массив

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let title = "New projec ";
-let screens = "Простые, Сложные, Интерактивные";
+let title = 'New projec';
+let screens = 'Простые, Сложные, Интерактивные';
 let screenPrice = 548;
 let rollback = 20;
 let fullPrice = 99999;
 let adaptive = true;
-alert("hello world");
+//alert("hello world");
 console.log("hi");
-//тест для отката
+//----------lesson2---------------
+console.log(typeof (title));
+console.log(typeof (fullPrice));
+console.log(typeof (adaptive));
+console.log('Длинна строки ' + screens.length);
+
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback / 100));
