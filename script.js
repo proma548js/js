ab2bfc6fada956a6cffaf35eeffd207a0bc5cbe9
Evgeniu <traingloo24@gmail.com>
2022-03-27 11:18:56 +0300

    Исправление дз2 Разбил массив на элементы

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let rollback = 20;
 let fullPrice = 99999;
 let adaptive = true;
 //alert("hello world");
-console.log("hi");
+//console.log("hi");
 //----------lesson2---------------
 console.log(typeof (title));
 console.log(typeof (fullPrice));
 console.log(typeof (adaptive));
-console.log(screens.toLowerCase().split());
+console.log(screens.toLowerCase().split(" "));
 console.log("Стоимость верстки экранов в рублях:", screenPrice, "Стоимость верстки экранов в долларах:", screenPrice / 3.2, "Стоимость верстки экранов в юанях:", screenPrice / 5);
 console.log("Стоимость разработки сайта в рублях:", fullPrice, "Стоимость разработки сайта в долларах:", fullPrice / 3.2, "Стоимость разработки сайта в юанях:", fullPrice / 5);
 console.log(screens.toLowerCase());
